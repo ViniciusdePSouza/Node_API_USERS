ffb86fec1abd11ebf9a1b150ad11bffee4467898
Vinícius Souza <dev.souzavinicius@gamil.com>
2023-12-11 14:17:54 -0300

    feature: getUsersController added

---
+++ src/controllers/getUsers/getUsers.js
@@ -0,0 +1,21 @@
+export class GetUsersController {
+  constructor(getUsersRepository) {
+    this.getUsersRepository = getUsersRepository;
+  }
+
+  async handle() {
+    try {
+      const users = await this.getUsersRepository.getUsers();
+
+      return {
+        statusCode: 200,
+        body: users
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "something went wrong",
+      };
+    }
+  }
+}
