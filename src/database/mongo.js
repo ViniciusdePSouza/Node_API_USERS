bd18aea4fd713415667e68bdcc7788d94e1f6758
Vinícius Souza <dev.souzavinicius@gamil.com>
2023-12-11 15:01:44 -0300

    feature: connection with mongo db

---
+++ src/database/mongo.js
@@ -0,0 +1,26 @@
+const { MongoClient, Db } = require("mongodb");
+
+const Mongo = {
+  client: undefined,
+  db: undefined,
+
+  async connect() {
+    const url = process.env.MONGO_URL;
+    const username = process.env.MONGO_USERNAME;
+    const password = process.env.MONGO_PASSWORD;
+
+    const client = new MongoClient(url, {
+      auth: { username: username, password: password },
+    });
+
+    const db = client.db('users-db');
+
+    this.client = client;
+    this.db = db;
+
+    console.log('connected to Mongo')
+  },
+};
+
+
+module.exports = Mongo
--- src/server.js
+++ src/server.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const Mongo = require("./database/mongo");
 
+const main = async () => {
+  dotenv.config();
+  
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
 
-dotenv.config();
-const app = express();
-app.use(cors());
-app.use(express.json());
+  const PORT = process.env.PORT || 8000;
+  await Mongo.connect();
 
+  app.get("/users", async (req, res) => {
+    const MongoGetUsersRepository = require("./repositories/getUsers/MongoGetUsers");
+    const GetUsersController = require("./controllers/getUsers/getUsers");
 
-const PORT = process.env.PORT || 8000;
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+    const getUserController = new GetUsersController(mongoGetUsersRepository);
 
-app.get("/users", async (req, res) => {
-  const MongoGetUsersRepository = require("./repositories/getUsers/MongoGetUsers");
-  const GetUsersController = require("./controllers/getUsers/getUsers");
+    const { body, statusCode } = await getUserController.handle();
 
-  const mongoGetUsersRepository = new MongoGetUsersRepository();
-  const getUserController = new GetUsersController(mongoGetUsersRepository);
+    res.status(statusCode).send(body);
+  });
 
-  const {body, statusCode} = await getUserController.handle()
+  app.listen(PORT, () => console.log("listening on port", PORT));
+};
 
-  res.status(statusCode).send(body)
-});
-
-app.listen(PORT, () => console.log("listening on port", PORT));
+main();
