6131a5b0e2649a41dd7112d88ff43e879b124f94
Vinícius Souza <dev.souzavinicius@gamil.com>
2023-12-12 11:36:32 -0300

    updateUserRepository added

---
+++ src/controllers/patchUser/patchUser.js
@@ -0,0 +1,5 @@
+class patchUserController {
+
+}
+
+module.exports = patchUserController
---
+++ src/repositories/patchUser/MongoPatchUser.js
@@ -0,0 +1,21 @@
+const Mongo = require("../../database/mongo");
+const { newObjectId } = require("mongodb");
+
+class MongoPatchUserRepository {
+  async updateUser(params, id) {
+    await Mongo.db.collection("users").updateOne(
+      { _id: newObjectId(id) },
+      {
+        $set: {
+          ...params,
+        },
+      }
+    );
+
+    const user = await Mongo.db.collection("users").findOne({ _id: newObjectId(id)})
+
+    if(!user) throw new Error('User not updated')
+
+    return user
+  }
+}
